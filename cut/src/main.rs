04c5b9c1066ee56cb13e061adf6ce462a41fdaf4
Tevin Thuku <tevinthuku@gmail.com>
2024-01-23 08:02:33 +0300

    read multiple fields

--- cut/src/main.rs
+++ cut/src/main.rs
@@ -13,44 +13,69 @@ fn main() -> Result<(), Box<dyn Error>> {
     if !command.starts_with(FILE_COMMAND) {
         return Err("Invalid command".into());
     }
-    // we are subtracting 1 because the command is 1 based and the field is 0 based
-    let field_needed = command.replace(FILE_COMMAND, "").parse::<usize>()? - 1;
+    let fields_needed = {
+        let fields_needed = command.replace(FILE_COMMAND, "");
+        if fields_needed.contains(',') {
+            fields_needed
+                .split(',')
+                .map(|field| field.parse::<usize>())
+                .collect::<Result<Vec<_>, _>>()?
+        } else if fields_needed.contains(' ') {
+            fields_needed
+                .split(' ')
+                .map(|field| field.parse::<usize>())
+                .collect::<Result<Vec<_>, _>>()?
+        } else {
+            vec![fields_needed.parse::<usize>()?]
+        }
+    };
     const DELIMETER_COMMAND: &str = "-d";
-    let maybe_file_name_or_delimeter = args.next().ok_or("Failed to get the file name")?;
+    let maybe_file_name_or_delimeter = args.next().ok_or("Failed to get the next argument")?;
     if maybe_file_name_or_delimeter.starts_with(DELIMETER_COMMAND) {
         let delimeter = maybe_file_name_or_delimeter.replace(DELIMETER_COMMAND, "");
         let file_name = args.next().ok_or("Failed to get the file name")?;
-        return process_command_f2(&file_name, &delimeter, field_needed);
+        return process_command_f2(&file_name, &delimeter, fields_needed);
     }
     let default_tab_delimeter = "\t";
     process_command_f2(
         &maybe_file_name_or_delimeter,
         default_tab_delimeter,
-        field_needed,
+        fields_needed,
     )
 }
 
 fn process_command_f2(
     file_name: &str,
     delimeter: &str,
-    field_needed: usize,
+    fields_needed: Vec<usize>,
 ) -> Result<(), Box<dyn Error>> {
     let f = File::open(file_name)?;
     let reader = BufReader::new(f);
     let mut stdout = io::stdout().lock();
 
-    let lines = reader.lines();
-    for line in lines {
-        let line = line?;
-        let word_needed = line.split(delimeter).nth(field_needed).unwrap_or_default();
-        let word_needed = format!("{word_needed}\n");
-        if let Err(err) = stdout.write_all(word_needed.as_bytes()) {
+    let mut write_to_output = |content: &[u8]| -> Result<(), Box<dyn Error>> {
+        if let Err(err) = stdout.write_all(content) {
             if err.kind() == io::ErrorKind::BrokenPipe {
                 return Ok(());
             } else {
                 return Err(err.into());
             }
         }
+        Ok(())
+    };
+
+    let lines = reader.lines();
+    for line in lines {
+        let line = line?;
+        for field_needed in &fields_needed {
+            // we are subtracting 1 because the command is 1 based and the field is 0 based
+            let field_needed = field_needed - 1;
+            let word_needed = line.split(delimeter).nth(field_needed).unwrap_or_default();
+            let word_needed = format!("{word_needed}{delimeter}");
+            write_to_output(word_needed.as_bytes())?;
+        }
+        write_to_output(b"\n")?;
     }
+
     Ok(())
 }
