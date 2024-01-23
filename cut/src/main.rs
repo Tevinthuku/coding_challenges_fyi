87f4fbd33839b5c2d0bfb753cc7c37bbf6236d62
Tevin Thuku <tevinthuku@gmail.com>
2024-01-23 07:42:11 +0300

    handle delimeter

--- cut/src/main.rs
+++ cut/src/main.rs
@@ -15,11 +15,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     // we are subtracting 1 because the command is 1 based and the field is 0 based
     let field_needed = command.replace(FILE_COMMAND, "").parse::<usize>()? - 1;
-    let file_name = args.next().ok_or("Failed to get the file name")?;
-    process_command_f2(&file_name, field_needed)
+    const DELIMETER_COMMAND: &str = "-d";
+    let maybe_file_name_or_delimeter = args.next().ok_or("Failed to get the file name")?;
+    if maybe_file_name_or_delimeter.starts_with(DELIMETER_COMMAND) {
+        let delimeter = maybe_file_name_or_delimeter.replace(DELIMETER_COMMAND, "");
+        let file_name = args.next().ok_or("Failed to get the file name")?;
+        return process_command_f2(&file_name, &delimeter, field_needed);
+    }
+    let default_tab_delimeter = "\t";
+    process_command_f2(
+        &maybe_file_name_or_delimeter,
+        default_tab_delimeter,
+        field_needed,
+    )
 }
 
-fn process_command_f2(file_name: &str, field_needed: usize) -> Result<(), Box<dyn Error>> {
+fn process_command_f2(
+    file_name: &str,
+    delimeter: &str,
+    field_needed: usize,
+) -> Result<(), Box<dyn Error>> {
     let f = File::open(file_name)?;
     let reader = BufReader::new(f);
     let mut stdout = io::stdout().lock();
@@ -27,9 +42,15 @@ fn process_command_f2(file_name: &str, field_needed: usize) -> Result<(), Box<dy
     let lines = reader.lines();
     for line in lines {
         let line = line?;
-        let second_word = line.split('\t').nth(field_needed).unwrap_or_default();
-        stdout.write_all(second_word.as_bytes())?;
-        stdout.write_all(b"\n")?;
+        let word_needed = line.split(delimeter).nth(field_needed).unwrap_or_default();
+        let word_needed = format!("{word_needed}\n");
+        if let Err(err) = stdout.write_all(word_needed.as_bytes()) {
+            if err.kind() == io::ErrorKind::BrokenPipe {
+                return Ok(());
+            } else {
+                return Err(err.into());
+            }
+        }
     }
     Ok(())
 }
