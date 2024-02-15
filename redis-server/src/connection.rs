ee180169dd767fde8d76f459ac38c00c18da40b8
Tevin Thuku <tevinthuku@gmail.com>
2024-02-15 16:19:18 +0300

    write once to the TCPStream
    redis-benchmark -t SET,GET -q
    WARNING: Could not fetch server CONFIG
    SET: 130208.34 requests per second, p50=0.207 msec
    GET: 132626.00 requests per second, p50=0.199 msec

--- redis-server/src/connection.rs
+++ redis-server/src/connection.rs
@@ -1,24 +1,22 @@
 use std::{
-    io::{self, BufWriter, Write},
+    io::{self, Write},
     net::TcpStream,
 };
 
 use anyhow::Context;
-use bytes::BytesMut;
+use bytes::{BufMut, BytesMut};
 
 use std::io::Read;
 
 use crate::frame::Frame;
 
 pub struct Connection {
-    stream: BufWriter<TcpStream>,
+    stream: TcpStream,
 }
 
 impl Connection {
     pub fn new(socket: TcpStream) -> Self {
-        Self {
-            stream: BufWriter::new(socket),
-        }
+        Self { stream: socket }
     }
 
     pub fn read_bytes(&mut self) -> anyhow::Result<Option<BytesMut>> {
@@ -26,7 +24,6 @@ impl Connection {
 
         let n = self
             .stream
-            .get_ref()
             .read(&mut data)
             .context("Failed to read buffer")?;
 
@@ -34,7 +31,7 @@ impl Connection {
             return Ok(None);
         }
 
-        Ok(Some(BytesMut::from(&data[..])))
+        Ok(Some(BytesMut::from(&data[..n])))
     }
 
     pub fn send_error(&mut self, err: &str) -> io::Result<()> {
@@ -43,53 +40,77 @@ impl Connection {
     }
 
     pub fn write_frame(&mut self, frame: Frame) -> io::Result<()> {
-        match frame {
+        let response = match frame {
             Frame::SimpleString(content) => {
-                self.stream.write_all(&[b'+'])?;
-                self.stream.write_all(content.as_bytes())?;
-                self.stream.write_all(b"\r\n")?;
+                let mut response = BytesMut::new();
+                response.put_u8(b'+');
+                response.extend(content.as_bytes());
+                response.put_u8(b'\r');
+                response.put_u8(b'\n');
+                response
             }
             Frame::Error(content) => {
-                self.stream.write_all(&[b'-'])?;
-                self.stream.write_all(content.as_bytes())?;
-                self.stream.write_all(b"\r\n")?;
+                let mut response = BytesMut::new();
+                response.put_u8(b'-');
+                response.extend(content.as_bytes());
+                response.put_u8(b'\r');
+                response.put_u8(b'\n');
+                response
             }
             Frame::BulkString(bytes) => {
-                let length = bytes.len();
-                self.stream.write_all(&[b'$'])?;
-                self.stream.write_all(length.to_string().as_bytes())?;
-                self.stream.write_all(b"\r\n")?;
-                self.stream.write_all(&bytes)?;
-                self.stream.write_all(b"\r\n")?;
+                let mut response = BytesMut::new();
+                response.put_u8(b'$');
+                response.extend(bytes.len().to_string().as_bytes());
+                response.put_u8(b'\r');
+                response.put_u8(b'\n');
+                response.extend(&bytes);
+                response.put_u8(b'\r');
+                response.put_u8(b'\n');
+                response
             }
             Frame::Boolean(bool) => {
-                self.stream.write_all(&[b'#'])?;
+                let mut response = BytesMut::new();
+                response.put_u8(b'#');
                 let val = if bool { b't' } else { b'f' };
-                self.stream.write_all(&[val])?;
-                self.stream.write_all(b"\r\n")?;
+                response.put_u8(val);
+                response.put_u8(b'\r');
+                response.put_u8(b'\n');
+                response
             }
             Frame::Integer(val) => {
-                self.stream.write_all(&[b':'])?;
+                let mut response = BytesMut::new();
+                response.put_u8(b':');
                 if val < 0 {
-                    self.stream.write_all(&[b'-'])?;
+                    response.put_u8(b'-');
                 }
-                self.stream.write_all(val.to_string().as_bytes())?;
-                self.stream.write_all(b"\r\n")?;
+                response.extend(val.to_string().as_bytes());
+                response.put_u8(b'\r');
+                response.put_u8(b'\n');
+                response
             }
             Frame::Double(val) => {
+                let mut response = BytesMut::new();
+                response.put_u8(b',');
                 self.stream.write_all(&[b','])?;
+
                 if val < 0.0 {
-                    self.stream.write_all(&[b'-'])?;
+                    response.put_u8(b'-');
                 }
-                self.stream.write_all(val.to_string().as_bytes())?;
-                self.stream.write_all(b"\r\n")?;
+                response.extend(val.to_string().as_bytes());
+                response.put_u8(b'\r');
+                response.put_u8(b'\n');
+                response
             }
             Frame::Null => {
-                self.stream.write_all(&[b'_', b'\r', b'\n'])?;
+                let mut response = BytesMut::with_capacity(3);
+                response.put_u8(b'_');
+                response.put_u8(b'\r');
+                response.put_u8(b'\n');
+                response
             }
             _ => unimplemented!(),
-        }
+        };
 
-        self.stream.flush()
+        self.stream.write_all(&response)
     }
 }
