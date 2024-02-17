2e6181c846c0ff7dbaf49d4a76232606ea1c3ae7
Tevin Thuku <tevinthuku@gmail.com>
2024-02-17 12:57:32 +0300

    fix parsing

--- redis-server/src/connection.rs
+++ redis-server/src/connection.rs
@@ -89,10 +89,6 @@ impl Connection {
                 let mut response = BytesMut::new();
                 response.put_u8(b',');
                 self.stream.write_all(&[b','])?;
-
-                if val < 0.0 {
-                    response.put_u8(b'-');
-                }
                 response.extend(val.to_string().as_bytes());
                 response.put_u8(b'\r');
                 response.put_u8(b'\n');
