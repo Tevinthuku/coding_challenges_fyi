579c59d38a722372c88038b6c00f96c4e01e8737
Tevin Thuku <tevinthuku@gmail.com>
2024-02-18 17:09:10 +0300

    purge task

--- redis-server/src/db.rs
+++ redis-server/src/db.rs
@@ -1,27 +1,54 @@
 use bytes::Bytes;
+use log::debug;
 use std::{
-    collections::HashMap,
-    io::{self},
+    collections::{BTreeSet, HashMap},
+    io,
     sync::{Arc, Mutex},
+    time::{Duration, Instant},
 };
+use tokio::sync::Notify;
 
 #[derive(Debug, Clone)]
 pub struct Db {
-    data: Arc<Mutex<Data>>,
+    inner: Arc<DbInner>,
+}
+
+impl Drop for Db {
+    fn drop(&mut self) {
+        self.inner.shutdown_purge_task();
+    }
+}
+
+#[derive(Debug)]
+struct DbInner {
+    data: Mutex<Data>,
+    background_task: Notify,
 }
 
 #[derive(Debug)]
 struct Data {
     inner: HashMap<String, Bytes>,
+    /// A set of keys that have an expiry time
+    expiry: BTreeSet<(Instant, String)>,
+    /// True when the Db instance is shutting down. This happens when all `Db`
+    /// values drop. Setting this to `true` signals to the background task to
+    /// exit.
+    shutdown: bool,
 }
 
 impl Default for Db {
     fn default() -> Self {
-        Self {
-            data: Arc::new(Mutex::new(Data {
+        let db_inner = DbInner {
+            data: Mutex::new(Data {
                 inner: HashMap::new(),
-            })),
-        }
+                expiry: BTreeSet::new(),
+                shutdown: false,
+            }),
+            background_task: Notify::new(),
+        };
+        let inner = Arc::new(db_inner);
+        tokio::spawn(purge_expired_tasks(inner.clone()));
+        Self { inner }
     }
 }
 
@@ -34,7 +61,7 @@ impl Db {
     where
         F: FnOnce(&mut HashMap<String, Bytes>) -> T,
     {
-        f(&mut self.data.lock().unwrap().inner)
+        f(&mut self.inner.data.lock().unwrap().inner)
     }
 
     /// The integer result from the closure is the new value for the key
@@ -89,4 +116,106 @@ impl Db {
             Ok(new_value)
         })
     }
+
+    pub fn set(&self, key: String, value: Bytes, expire: Option<Duration>) {
+        let mut state = self.inner.data.lock().unwrap();
+
+        let mut notify = false;
+
+        let expires_at = expire.map(|duration| {
+            let when = Instant::now() + duration;
+
+            notify = state
+                .expiry
+                .iter()
+                .next()
+                .map(|(current, _)| *current > when)
+                .unwrap_or(true);
+
+            when
+        });
+
+        let prev = state.inner.insert(key.clone(), value);
+
+        if let Some(_prev) = prev {
+            if let Some(expires_at) = expires_at {
+                state.expiry.remove(&(expires_at, key.clone()));
+            }
+        }
+
+        if let Some(when) = expires_at {
+            state.expiry.insert((when, key));
+        }
+
+        drop(state);
+
+        if notify {
+            // Finally, only notify the background task if it needs to update
+            // its state to reflect a new expiration.
+            self.inner.background_task.notify_one();
+        }
+
+        todo!()
+    }
+}
+
+impl DbInner {
+    fn is_shutdown(&self) -> bool {
+        self.data.lock().unwrap().shutdown
+    }
+
+    /// Signals the purge background task to shut down. This is called by the
+    /// `DbShutdown`s `Drop` implementation.
+    fn shutdown_purge_task(&self) {
+        // The background task must be signaled to shut down. This is done by
+        // setting `Data::shutdown` to `true` and signalling the task.
+        let mut state = self.data.lock().unwrap();
+        state.shutdown = true;
+
+        // Drop the lock before signalling the background task. This helps
+        // reduce lock contention by ensuring the background task doesn't
+        // wake up only to be unable to acquire the mutex.
+        drop(state);
+
+        self.background_task.notify_one();
+    }
+
+    fn purge_expired_keys(&self) -> Option<Instant> {
+        let mut data = self.data.lock().unwrap();
+        if data.shutdown {
+            // The database is shutting down. All handles to the shared state
+            // have dropped. The background task should exit.
+            return None;
+        }
+        let now = Instant::now();
+
+        while let Some((when, key)) = data.expiry.iter().next().cloned() {
+            if when > now {
+                // Done purging, `when` is the instant at which the next key
+                // expires. The worker task will wait until this instant.
+                return Some(when);
+            }
+
+            data.inner.remove(&key);
+            data.expiry.remove(&(when, key));
+        }
+
+        None
+    }
+}
+
+async fn purge_expired_tasks(shared: Arc<DbInner>) {
+    while !shared.is_shutdown() {
+        if let Some(when) = shared.purge_expired_keys() {
+            tokio::select! {
+                _ = tokio::time::sleep_until(when.into()) => {}
+                _ = shared.background_task.notified() => {}
+            }
+        } else {
+            // There are no keys to expire, so we wait until we are notified
+            shared.background_task.notified().await;
+        }
+    }
+
+    debug!("Background task is shutting down");
 }
